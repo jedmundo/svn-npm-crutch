da2c03c174505c014522ee3a9d58bd5c2227b164
jedmundo <joao.a.edmundo@gmail.com>
2016-07-04 09:25:29 +0200

    logging for npm install function

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-console.log( "You can't use svn-npm-crutch programmatically!" );
-module.exports = function() {
-	console.log( "You can't use svn-npm-crutch programmatically!" );
-};
+console.log("You can't use svn-npm-crutch programmatically!");
+// module.exports = function () {
+	console.log("You can't use svn-npm-crutch programmatically!");
+
+	try {
+		var fs = require('fs');
+		var result = fs.accessSync("package2.json", fs.R_OK | fs.W_OK);
+		console.log('FOUND',result);
+    } catch (e) {
+		//error
+		console.log('Error: ' + e);
+    }
+// };
